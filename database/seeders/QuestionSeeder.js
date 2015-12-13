f5beaf670d27eefba9cb5cb6bc867ca5af0cc91b
Chris Normansell <chris.normansell@gmail.com>
2015-12-13 20:10:01 +0000

    Added question seeder

---
+++ database/seeders/QuestionSeeder.js
@@ -0,0 +1,28 @@
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('database/quiz.sqlite');
+var fs = require('fs');
+
+
+db.serialize(function () {
+  db.run('begin transaction');
+  var stmt = db.prepare('INSERT INTO questions VALUES (?, ?, ?, ?, ?)');
+  var questionsString = fs.readFileSync(__dirname + '/data/multi-choice.json').toString();
+  var categories = JSON.parse(questionsString);
+  for (var category in categories) {
+    var questions = categories[category];
+    for (var index in questions) {
+      var question = questions[index];
+      stmt.run([
+        question.question,
+        JSON.stringify(question.answers),
+        question.correctAnswer,
+        question.difficulty,
+        category
+      ]);
+    }
+  }
+  stmt.finalize();
+  db.run('commit');
+});
+
+db.close();
