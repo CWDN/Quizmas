f93ec411d2d9d8cb101b1eb872212255fab8af8b
Chris Normansell <chris.normansell@gmail.com>
2015-12-14 09:43:21 +0000

    Fixed MR conflicts and added body parser middleware

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var exphs = require('express-handlebars');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.engine('handlebars', exphs({defaultLayout: 'master'}));
 app.set('view engine', 'handlebars');
+
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(require('./controllers'));
 
 var ipAddress = require('./helpers/IpFinder')();
