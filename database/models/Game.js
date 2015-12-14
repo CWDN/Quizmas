8a0112595c4ca705005c3c11c35d0847ed167caf
Chris Normansell <chris.normansell@gmail.com>
2015-12-14 10:41:25 +0000

    Built the getByName function

--- database/models/Game.js
+++ database/models/Game.js
@@ -22,12 +22,51 @@ Game.prototype.create = function () {
   db.close();
 };
 
+/**
+ * Imports the data from the given object into this game object.
+ * @param  {Object} object
+ * @return {Game}
+ */
+Game.prototype.importFromDBObject = function (object) {
+  if (object.name !== undefined) {
+    this.setName(object.name);
+    this.fromDatabase = true;
+  }
+  return this;
+};
+
+/**
+ * Gets a game from the name given.
+ * @param  {string} name
+ * @return {Game|undefined}
+ */
 Game.getByName = function (name) {
   var db = getDBConnection();
+  var result;
+  var sync = true;
   db.serialize(function () {
-    db.run('CREATE TABLE game (name TEXT)');
+    var stmt = db.prepare('SELECT * FROM games WHERE name=?');
+    stmt.run(name);
+    stmt.all(function (err, res) {
+      if (err) throw err;
+      result = res;
+    });
+    stmt.finalize(function () {
+      sync = false;
+      db.close();
+    });
   });
-  db.close();
+  while(sync) {
+    require('deasync').sleep(100);
+  }
+  if (result.length > 0) {
+    result = result.pop();
+  } else {
+    return undefined;
+  }
+  var game = new Game();
+  game.importFromDBObject(result);
+  return game;
 };
 
 function getDBConnection () {
