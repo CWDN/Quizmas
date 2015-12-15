3e7243180c2d17fc00a785a52a62b310985e2867
Chris Normansell <chris.normansell@gmail.com>
2015-12-15 14:50:20 +0000

    Creating a new quiz and getting question from quiz

--- controllers/lobby-ws.js
+++ controllers/lobby-ws.js
@@ -2,12 +2,18 @@ var server = require('../server');
 var io = server.io;
 var app = server.app;
 var Team = require('../database/models/Team');
+var Quiz = require('../quiz');
 
 function Lobby (game) {
   this.game = game;
 
   this.startWebSockets = function () {
     var nsp = io.of('/' + this.game.getName());
+    var quiz = new Quiz(['General Knowledge'], {
+      hard: 5,
+      medium: 10,
+      easy: 15
+    }, game);
     nsp.on('connection', function (socket) {
       socket.on('join', function (data) {
         Team.getByTeamAndGame(data.team, data.game, function (team) {
@@ -32,25 +38,33 @@ function Lobby (game) {
       });
 
       socket.on('next-question', function () {
-        nextQuestion(['Answer1', 'Answer2']);
+        var state = quiz.getNextQuestion(function (question) {
+          app.render('question', {
+            layout: false,
+            answers: question.getOptions(),
+            question: question.getQuestion()
+          }, function (err, html) {
+            if (err) {
+              console.log(err);
+            }
+            nsp.emit('page', {
+              html: html
+            });
+          });
+        });
+
+        if (state === 'EndQuiz') {
+
+        }
+        if (state === 'EndCategory') {
+
+        }
       });
 
       socket.on('send-answer', function (data) {
         console.log(data);
-        nextQuestion(['ONE', 'TWO', 'THREE', 'FOUR']);
       });
     });
-
-    function nextQuestion (answers) {
-        app.render('question', {layout: false, answers: answers}, function (err, html) {
-          if (err) {
-            console.log(err);
-          }
-          nsp.emit('page', {
-            html: html
-          });
-        });
-    }
   };
 
   this.startWebSockets();
